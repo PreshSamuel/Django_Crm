bb3aea0a17640e8e9c07c4b48ed528ab7339a24f
PreshSamuel <141229061+PreshSamuel@users.noreply.github.com>
2024-09-20 13:21:14 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-epw9*yd4n%zc-(v^c+gi^xxbs0ptfv*s-la+4^-m_k!80f-xy@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '*.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '*.onrender.com', "django-crm-rxr1.onrender.com"]
 
 
 # Application definition
